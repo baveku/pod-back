8dbb976a64774bc90a2365869fbf40da42cafbcf
bachvq <baveku@gmail.com>
2022-12-10 19:32:03 +0700

    fix build

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Vuetify
-import { router } from '@/router'
+import { router } from './router'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
