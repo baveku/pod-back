7d3bbc5d6bbd689344a6d3f4f2594f412353ae00
jbach <baveku@gmail.com>
2022-12-28 21:04:14 +0700

    close all active doc

--- python/main.py
+++ python/main.py
@@ -10,20 +10,19 @@ ORDER_IDS = args[1].split(',')
 TEXT_INPUTS = args[2].split(',')
 SKU = args[3]
 
-OUTPUT_DIR = Path.joinpath(Path(PSD_PATH).parent.parent, 'POD-BACK')
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-SKU_FOLDER_DIR = Path.joinpath(OUTPUT_DIR, SKU)
-os.makedirs(SKU_FOLDER_DIR, exist_ok=True)
-
 psApp = win32com.client.Dispatch("Photoshop.Application")
-doc = psApp.Open(PSD_PATH)
-layers = psApp.Application.ActiveDocument
+try:
+	OUTPUT_DIR = Path.joinpath(Path(PSD_PATH).parent.parent, 'POD-BACK')
+	os.makedirs(OUTPUT_DIR, exist_ok=True)
+	SKU_FOLDER_DIR = Path.joinpath(OUTPUT_DIR, SKU)
+	os.makedirs(SKU_FOLDER_DIR, exist_ok=True)
+	doc = psApp.Open(PSD_PATH)
+	layers = psApp.Application.ActiveDocument
 
-for idx, val in enumerate(ORDER_IDS):
-	file_export_name = ORDER_IDS[idx]
-	replace_text = TEXT_INPUTS[idx]
-	isMultiContent = len(replace_text.split('|')) > 1
-	try:
+	for idx, val in enumerate(ORDER_IDS):
+		file_export_name = ORDER_IDS[idx]
+		replace_text = TEXT_INPUTS[idx]
+		isMultiContent = len(replace_text.split('|')) > 1
 		if isMultiContent == False:
 			quote_layer = layers.ArtLayers["USER_CONTENT_NAME"]
 			quote_layer.TextItem.contents = replace_text.strip()
@@ -32,13 +31,17 @@ for idx, val in enumerate(ORDER_IDS):
 			for ind, text in enumerate(texts):
 				quote_layer = layers.ArtLayers["USER_CONTENT_NAME_{ind}".format(ind=ind)]
 				quote_layer.TextItem.contents = replace_text.strip()
-	except:
-		print("An exception occurred")
 
-	options = win32com.client.Dispatch('Photoshop.PNGSaveOptions')
-	fileName = file_export_name + "-" + replace_text + ".png"
-	ourDir = SKU_FOLDER_DIR
-	fileExport = Path.joinpath(ourDir, fileName)
-	doc.SaveAs(fileExport, options, True)
-doc.Close(2)
+
+		options = win32com.client.Dispatch('Photoshop.PNGSaveOptions')
+		fileName = file_export_name + "-" + replace_text + ".png"
+		ourDir = SKU_FOLDER_DIR
+		fileExport = Path.joinpath(ourDir, fileName)
+		doc.SaveAs(fileExport, options, True)
+	doc.Close(2)
+except:
+	print("An exception occurred")
+
+while psApp.Documents.Count > 0:
+    psApp.ActiveDocument.Close(2)
 sys.stdout.flush()
